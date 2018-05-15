ce25c09e4d68bb37e3037c6bb67a8bc4dc71d6a2
Morioka Shuhei <selmertsx@gmail.com>
2018-05-15 10:26:33 +0900

    create base for index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,29 @@ import {
   APIGatewayProxyResult,
   Context
 } from "aws-lambda";
+
+import { ISlackEvent, ISlackEventCallback } from "./slack";
+
+function buildResponse(statusCode: number, body?): APIGatewayProxyResult {
+  return { statusCode, body: JSON.stringify(body) };
+}
+
+export async function createIssue(
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  if (!event.body) {
+    return buildResponse(400);
+  }
+
+  const payload: ISlackEvent = JSON.parse(event.body);
+  if (event.headers["X-Slack-Retry-Reason"] === "http_timeout") {
+    return buildResponse(200, { status: "OK" });
+  }
+
+  if (payload.type === "url_verification") {
+    return buildResponse(200, { challenge: payload.challenge });
+  }
+
+  return buildResponse(200);
+}
