a9cff1506eda4cec96c4715fd9fc33fe9a9b4e26
Morioka Shuhei <selmertsx@gmail.com>
2018-05-15 10:46:22 +0900

    create issuer class based on octokit

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import {
   Context
 } from "aws-lambda";
 
+import { Issuer } from "./issuer";
 import { ISlackEvent, ISlackEventCallback } from "./slack";
 
 function buildResponse(statusCode: number, body?): APIGatewayProxyResult {
@@ -18,15 +19,12 @@ export async function createIssue(
   if (!event.body) {
     return buildResponse(400);
   }
-
   const payload: ISlackEvent = JSON.parse(event.body);
   if (event.headers["X-Slack-Retry-Reason"] === "http_timeout") {
     return buildResponse(200, { status: "OK" });
   }
-
   if (payload.type === "url_verification") {
     return buildResponse(200, { challenge: payload.challenge });
   }
-
   return buildResponse(200);
 }
---
+++ src/issuer.ts
@@ -0,0 +1,28 @@
+import octokit from "@octokit/rest";
+
+export class Issuer {
+  private readonly octokit: any;
+  private readonly owner: string;
+  private readonly repo: string;
+
+  constructor(owner: string, repo: string) {
+    this.octokit = new octokit();
+    this.owner = owner;
+    this.repo = repo;
+  }
+
+  public authenticate(): void {
+    return this.octokit.authenticate({
+      type: "token",
+      token: process.env.Token
+    });
+  }
+
+  public async create(title: string): Promise<any> {
+    return this.octokit.issues.create({
+      owner: this.owner,
+      repo: this.repo,
+      title
+    });
+  }
+}
